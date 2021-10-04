0997f19de03fc90da18dfe0990726f23b6963743
SFP <farazprottoy@gmail.com>
2021-10-04 11:20:35 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./App.css";
+import About from "./components/About/About";
+import Header from "./components/Header/Header";
+import Home from "./components/Home/Home";
+import Instructors from "./components/Instructors/Instructors";
+import NotFound from "./components/NotFound/NotFound";
+import Services from "./components/Services/Services";
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Header></Header>
+        <Switch>
+          <Route exact path="/">
+            <Home></Home>
+          </Route>
+          <Route path="/home">
+            <Home></Home>
+          </Route>
+          <Route path="/services">
+            <Services></Services>
+          </Route>
+          <Route path="/instructores">
+            <Instructors></Instructors>
+          </Route>
+          <Route path="/about">
+            <About></About>
+          </Route>
+          <Route path="*">
+            <NotFound></NotFound>
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    // <Container className="d-flex justify-content-between">
+    //   <Row>
+    //     <Col md={2}>GoAbroadEdu</Col>
+    //   </Row>
+    //   <Row>
+    //     <Col md={10}>
+    //       <ul>
+    //         <Link>
+    //           <li>Home</li>
+    //         </Link>
+    //         <li>Home</li>
+    //         <li>Home</li>
+    //       </ul>
+    //     </Col>
+    //   </Row>
+    // </Container>
+    <Navbar bg="light" variant="light">
+      <Container>
+        <Navbar.Brand to="/home">GoAbroadEdu</Navbar.Brand>
+        <Nav className="">
+          <Nav.Link>
+            <Link to="/home">Home</Link>
+          </Nav.Link>
+          <Nav.Link to="/services">Services</Nav.Link>
+          <Nav.Link to="/instructores">Instructors</Nav.Link>
+          <Nav.Link to="/about">About</Nav.Link>
+        </Nav>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default Header;
